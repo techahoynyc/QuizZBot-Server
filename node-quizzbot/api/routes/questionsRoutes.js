ccc6c24a879abee1d6ba5dc26b7c06f7f5418218
klinstifen <klinstifen@gmail.com>
2019-10-17 13:22:14 -0400

    Created questions.Routes.js

---
+++ node-quizzbot/api/routes/questionsRoutes.js
@@ -0,0 +1,14 @@
+'use strict';
+// question routes
+module.exports = function(app) {
+  var db = require('../controllers/queries');
+
+  app.route('/')
+   .get(db.getHome);
+  });
+
+  app.route('/questions/:qbid')
+    .get(db.getQbyQBID);
+    .post(db.saveAnswer);
+
+};
